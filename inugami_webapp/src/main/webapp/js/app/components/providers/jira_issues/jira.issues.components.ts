7ec3d83c76aa0d20ea80a306153e1d0e15c04fa3
Patrick Guillerm <pguillerm.prestataire@oui.sncf>
2019-04-11 13:30:31 +0200

    append styles on jira issues component

--- inugami_webapp/src/main/webapp/js/app/components/providers/jira_issues/jira.issues.components.ts
+++ inugami_webapp/src/main/webapp/js/app/components/providers/jira_issues/jira.issues.components.ts
@@ -13,6 +13,7 @@ export class JiraIssuesComponent implements OnInit, AfterContentInit{
     @Input() event                          : string;
     @Input() pluginName                     : string;
     @Input() transformer                    : any;
+    @Input() baseUrl                        : string;
     private issuesList                      : any[];
 
     /**************************************************************************
@@ -27,7 +28,6 @@ export class JiraIssuesComponent implements OnInit, AfterContentInit{
     /**************************************************************************
     * METHODS
     **************************************************************************/
-
     private injectData(event){
         if (isNotNull(event.detail.data) && isNotNull(event.detail.data.values)) {
             if(isNotNull(this.transformer)){
@@ -36,5 +36,21 @@ export class JiraIssuesComponent implements OnInit, AfterContentInit{
                 throw "jira transformer is mandatory";
             }
         }
+    }
+
+    public buildJiraUrl(issue){
+        let result = "#";
+        if(isNotNull(this.baseUrl) && isNotNull(issue.id)){
+            let buffer = [];
+            if(this.baseUrl.endsWith('/')){
+                buffer.push(this.baseUrl);
+            }else{
+                buffer.push(this.baseUrl);
+                buffer.push('/');
+            }
+            buffer.push(issue.id);
+            result = buffer.join('');
+        }
+        return result;
     }
 }
