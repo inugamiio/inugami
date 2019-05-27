fa9e6006d29f58a156b15c97b28884d84d9724a2
Gobin Jules <jgobin@oui.sncf>
2019-05-27 15:22:26 +0200

    removed comment

--- inugami_webapp/src/main/webapp/js/app/models/alert.entity.ts
+++ inugami_webapp/src/main/webapp/js/app/models/alert.entity.ts
@@ -25,19 +25,6 @@ export class AlertEntity {
         public activations      : ActivationTime[],
         public inverse          : boolean,
     ){ }
-    /*constructor(public alerteName       : string,
-        public level            : string,
-        public levelType        : string,
-        public levelNumber      : number,
-        public label            : string,
-        public subLabel         : string,
-        public channel          : string,
-        public duration         : number,
-        public created          : number,
-        public data             : string,
-        public enable           : boolean,
-        public ttl              : number,
-        public providers        : string[]){ }*/
 }
 
 
