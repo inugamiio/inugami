5d66a7c0b0d1074fa851de11ef0ad56f844bc267
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-02 20:32:54 +0200

    [#221] FIX:  fix Jackson java 8 date encoding in UnitTestHelper

--- inugami_commons_test/src/test/java/io/inugami/commons/test/UnitTestHelperTest.java
+++ inugami_commons_test/src/test/java/io/inugami/commons/test/UnitTestHelperTest.java
@@ -1,6 +1,20 @@
 package io.inugami.commons.test;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static io.inugami.commons.test.UnitTestHelper.assertText;
+import static io.inugami.commons.test.UnitTestHelper.assertTextRelatif;
 import static org.junit.jupiter.api.Assertions.*;
-class UnitTestHelperTest {
-  
+
+public class UnitTestHelperTest {
+
+
+    @Test
+    public void assertText_withLocalDateTime_shouldSerializeAsString() {
+        LocalDateTime date = LocalDateTime.of(2023, 4, 2, 20, 27, 0);
+        assertTextRelatif(date, "assertText_withLocalDateTime_shouldSerializeAsString.json");
+    }
 }
