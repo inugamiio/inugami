25f495c130e7eea60109f0c6997b06d76efb8e74
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-29 13:09:15 +0200

    #252 FEAT : Add toMap in Warning code

--- inugami_monitoring/inugami_monitoring_core/src/main/java/io/inugami/monitoring/core/interceptors/spi/DefaultWarningTracker.java
+++ inugami_monitoring/inugami_monitoring_core/src/main/java/io/inugami/monitoring/core/interceptors/spi/DefaultWarningTracker.java
@@ -4,9 +4,11 @@ import io.inugami.api.exceptions.Warning;
 import io.inugami.api.exceptions.WarningTracker;
 import io.inugami.api.monitoring.MdcService;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DefaultWarningTracker implements WarningTracker {
@@ -15,13 +17,15 @@ public class DefaultWarningTracker implements WarningTracker {
         final List<String> codes = new ArrayList<>(warning.stream().map(Warning::getWarningCode).collect(Collectors.toList()));
         Collections.sort(codes);
 
-        final String warningCode = String.join("_", codes);
-        final MdcService mdc      = MdcService.getInstance();
+        final String     warningCode = String.join("_", codes);
+        final MdcService mdc         = MdcService.getInstance();
 
         mdc.warning(warningCode);
 
-        for(Warning warn : warning){
-            mdc.setMdc("warn_"+warn.getWarningCode(), warn.getMessage());
+        for (final Warning warn : warning) {
+            for (final Map.Entry<String, Serializable> entry : warn.toMap().entrySet()) {
+                mdc.setMdc(entry.getKey(), entry.getValue());
+            }
         }
     }
 }
