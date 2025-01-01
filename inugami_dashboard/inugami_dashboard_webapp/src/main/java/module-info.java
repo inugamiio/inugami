43c420afd08dcf5c2456f13da8f8d307a725758a
Patrick Guillerm <pguillerm@smartwavesa.com>
2025-01-01 10:26:14 +0100

    #379 FEAT : migrate to JDK 17 and JakartaEE

---
+++ inugami_dashboard/inugami_dashboard_webapp/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/* --------------------------------------------------------------------
+ *  Inugami
+ * --------------------------------------------------------------------
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+module io.inugami.dashboard.webapp {
+    requires com.fasterxml.jackson.annotation;
+    requires lombok;
+    requires org.slf4j;
+
+    requires io.inugami.framework.interfaces;
+    requires io.inugami.framework.api;
+    requires io.inugami.dashboard.api;
+    requires io.inugami.dashboard.core;
+    requires io.inugami.dashboard.infrastructure;
+    requires io.inugami.dashboard.interfaces.api;
+    requires io.inugami.dashboard.interfaces.core;
+}
