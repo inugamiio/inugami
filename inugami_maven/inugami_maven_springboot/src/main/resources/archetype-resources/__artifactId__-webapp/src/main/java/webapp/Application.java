63197730dab39b60e5d258b3e989937e535581e0
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-08 10:52:51 +0100

    #429 FIX : fix send metrics to elastic search

--- inugami_maven/inugami_maven_springboot/src/main/resources/archetype-resources/__artifactId__-webapp/src/main/java/webapp/Application.java
+++ inugami_maven/inugami_maven_springboot/src/main/resources/archetype-resources/__artifactId__-webapp/src/main/java/webapp/Application.java
@@ -1,6 +1,5 @@
 package ${package}.webapp;
-
-import io.inugami.framework.commons.spring.EnabledInugami;
+import io.inugami.framework.commons.spring.configuration.ConfigConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
