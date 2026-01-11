168f5290e7d713c0bdf1e70a9c3ba7bccdee83e2
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-11 07:58:14 +0100

    #417 FIX : clean up

--- inugami_framework/inugami_api_interfaces/src/main/java/io/inugami/framework/interfaces/monitoring/logger/mapper/MdcDynamicFieldSPI.java
+++ inugami_framework/inugami_api_interfaces/src/main/java/io/inugami/framework/interfaces/monitoring/logger/mapper/MdcDynamicFieldSPI.java
@@ -2,7 +2,7 @@ package io.inugami.framework.interfaces.monitoring.logger.mapper;
 
 import java.io.Serializable;
 import java.util.Map;
-
+@FunctionalInterface
 public interface MdcDynamicFieldSPI {
     Map<String, Serializable> generate();
 }
